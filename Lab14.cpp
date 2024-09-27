0f90df32c6eb3d90b74f6f6df2e492463d0e32e0
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-09-26 18:54:20 -0700

    Timestamp

--- Lab14.cpp
+++ Lab14.cpp
@@ -14,6 +14,7 @@ private:
     int r;
     int g;
     int b;
+
 public:
     Color();
     Color(int, int, int);
@@ -21,6 +22,10 @@ public:
     void setG(int);
     void setB(int);
     void setColor(int, int, int);
+    int getR() const;
+    int getG() const;
+    int getB() const;
+    int toString() const; // print() method but returns a string
     ~Color();
 };
 
@@ -34,57 +39,74 @@ int main()
     vector<int> colorCodes = {}; // Initialize vector, will be destroyed and redefined during file read
     vector<int>::iterator it;    // Iterator to traverse array. Not really needed because each loop redeclares iterator anyways
     unsigned int index = 0;
-    // File to read from
-    const string INPUT_FILE_NAME = "colorCodes.txt";
-    ifstream inputFile;
 
-
-    // Try opening file and output error to console if file is no good
-    inputFile.open(INPUT_FILE_NAME);
-    
-    
     cout << endl; // Spacing
     return 0;
 }
 
-Color::Color(/* args */)
-: r(0), g(0), b(0) // Initialized default if none specified
+// Default constructor
+Color::Color()
+    : r(0), g(0), b(0) // Initialized default if none specified
 {
 }
 
+// Overloaded constructor
 Color::Color(int red, int green, int blue)
-: r(red), g(green), b(blue) // Initialized default if none specified
+    : r(red), g(green), b(blue) // Initialized default if none specified
 {
 }
 
-void Color::setR(int Red){
+void Color::setR(int Red)
+{
     if (Red >= 0 && Red <= 255)
     {
         r = Red;
-    } else {
+    }
+    else
+    {
         throw invalid_argument("Red value must be between 0 and 255.");
     }
 }
-void Color::setG(int Green){
+void Color::setG(int Green)
+{
     if (Green >= 0 && Green <= 255)
     {
         g = Green;
-    } else {
+    }
+    else
+    {
         throw invalid_argument("Green value must be between 0 and 255.");
     }
 }
-void Color::setB(int Blue){
+void Color::setB(int Blue)
+{
     if (Blue >= 0 && Blue <= 255)
     {
         b = Blue;
-    } else {
+    }
+    else
+    {
         throw invalid_argument("Blue value must be between 0 and 255.");
     }
 }
-void Color::setColor(int Red, int Green, int Blue){
+void Color::setColor(int Red, int Green, int Blue)
+{
     setR(Red);
     setG(Green);
     setB(Blue);
+}
+int Color::getR() const {
+    return r;
+}
+int Color::getR() const {
+    return r;
+}
+int Color::getR() const {
+    return r;
+}
+// I would probably call 
+string Color::print() const {
+    return r;
 }
 
 Color::~Color()
